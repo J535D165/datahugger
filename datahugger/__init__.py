e58c5969daf938f58989f6706f908ff440a0d726
Jonathan de Bruin <jonathandebruinos@gmail.com>
2023-02-28 09:42:00 +0100

    Happy lint

--- datahugger/__init__.py
+++ datahugger/__init__.py
@@ -1,6 +1,6 @@
 from datahugger.api import get
 from datahugger.api import info
-from datahugger.deprecated import load_repository
+from datahugger.deprecated import load_repository  # noqa
 
 __all__ = ["get", "info"]
 
