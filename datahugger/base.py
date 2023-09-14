beb81bf8241130a4f995727d522f013e0cbc9fca
Dave Tromp <2326631+davetromp@users.noreply.github.com>
2023-09-14 15:47:30 +0200

    Fix unzip option and documentation (#55)
    Closes #53 
    
    Co-authored-by: Dave Tromp <dave.tromp@surf.nl>
    Co-authored-by: Jonathan de Bruin <jonathandebruinos@gmail.com>

--- datahugger/base.py
+++ datahugger/base.py
@@ -346,7 +346,11 @@ class DatasetDownloader:
         output_folder: Union[Path, str],
         **kwargs,
     ):
-        if len(self.files) == 1 and self.files[0]["link"].endswith(".zip"):
+        if (
+            len(self.files) == 1
+            and self.files[0]["link"].endswith(".zip")
+            and self.unzip
+        ):
             self._unpack_single_folder(self.files[0]["link"], output_folder)
             return
 
