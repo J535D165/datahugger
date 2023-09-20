5a6f7a8ecbf53ef021b8147e0d8632c7e7cb3e9e
Jonathan de Bruin <jonathandebruinos@gmail.com>
2023-09-20 09:41:35 +0200

    Refactor DSpace

--- datahugger/services.py
+++ datahugger/services.py
@@ -235,7 +235,8 @@ class DSpaceDataset(DatasetDownloader):
     def _get_attr_link(self, record, base_url):
         return base_url + record["retrieveLink"]
 
-    def _pre_files(self):
+    @property
+    def API_URL_META(self):
         uri = urlparse(_get_url(self.resource))
         base_url = uri.scheme + "://" + uri.netloc
 
@@ -244,7 +245,7 @@ class DSpaceDataset(DatasetDownloader):
         res.raise_for_status()
 
         # set the API_URL_META
-        self.API_URL_META = base_url + res.json()["link"] + "/bitstreams"
+        return base_url + res.json()["link"] + "/bitstreams"
 
 
 class MendeleyDataset(DatasetDownloader):
